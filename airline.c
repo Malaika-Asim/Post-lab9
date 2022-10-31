6013b985a004acdc0f86a7cbf2cc325079490768
Malaika-Asim <malaikasim803@gmail.com>
2022-10-31 16:27:00 +0500

    airline.c

---
+++ airline.c
@@ -0,0 +1,84 @@
+#include<stdio.h>
+int arr[100];
+void bookseat1();
+void bookseat2();
+int count;
+int main()
+{
+	for (int i = 0; i < 100; i++)
+	{
+		arr[i] = 0;
+	}
+	printf("\t\t\t\t\tAirline Reservation System");
+	printf("\n\t\t\t*********************************************************");
+	printf("\n\nChoose from the given Options:-");
+	menu_ticket();
+}
+void bookseat1(void)
+{
+	//For First Class
+	printf("\nSelect type of class: 1"); 
+    int seat;
+    printf("\nSelect seat number(1-30): ");
+	scanf_s("%d", &seat);
+	if (seat >= 1 && seat <= 30)
+	{
+		if (arr[seat-1] == 0)
+		{
+			arr[seat - 1] = 1;
+			count = count + 1;
+			printf("********************************");
+			printf("\nYou type of class is First Class!");
+			printf("\nYour seat number is: %d", seat);
+			printf("\n********************************");
+		}
+		else if (arr[seat-1] == 1)
+		{
+			printf("This seat is already booked!");
+			printf("\nPlease book another seat!\n");
+			printf("********************************");
+			bookseat1();
+		}
+	}
+	else if (seat > 30)
+	{
+		printf("\n");
+		printf("Please type the correct seat number for First Class!");
+		bookseat1();
+	}
+	menu_ticket();
+}
+void bookseat2(void)
+{
+	//For Economy Class
+	printf("\nSelect type of class: 2");
+	int seat;
+	printf("\nSelect seat number(31-100): ");
+	scanf_s("%d", &seat);
+	if (seat >= 31 && seat <= 100)
+	{
+		if (arr[seat-1] == 0)
+		{ 
+			arr[seat - 1] = 1;
+			count = count + 1;
+			printf("********************************");
+			printf("\nYou type of class is Economy Class!");
+			printf("\nYour seat number is: %d", seat);
+			printf("\n********************************");
+		}
+		else if (arr[seat-1] == 1)
+		{
+			 printf("This seat is already booked!");
+			 printf("\nPlease book another seat!\n");
+			 printf("********************************");
+			 bookseat2();
+		}
+	}
+	else if (seat < 31)
+	{
+		printf("\n");
+		printf("Please type the correct seat number for Economy Class!");
+		bookseat2();
+	}
+	menu_ticket();
+}
